42a32484c5e18561e3c29901ab1c4123b40c68ae
Guimonto <36004939+Guimonto@users.noreply.github.com>
2018-02-13 21:56:14 +0100

    Tool Bar

--- app/src/main/java/sdm/pract1/whowantstobeamillionaire/MainActivity.java
+++ app/src/main/java/sdm/pract1/whowantstobeamillionaire/MainActivity.java
@@ -2,6 +2,8 @@ package sdm.pract1.whowantstobeamillionaire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return  true;
     }
 }
