e02053f16c36300f88cef03de9d7e2600a7469de
Guimonto <36004939+Guimonto@users.noreply.github.com>
2018-02-15 12:11:53 +0100

    Settings Activity

--- app/src/main/java/sdm/pract1/whowantstobeamillionaire/MainActivity.java
+++ app/src/main/java/sdm/pract1/whowantstobeamillionaire/MainActivity.java
@@ -1,9 +1,12 @@
 package sdm.pract1.whowantstobeamillionaire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +20,36 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+
+            case R.id.menu_about:
+                Intent about = new Intent(this, AboutActivity.class);
+                startActivity(about);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clickButtons(View v){
+        switch (v.getId()){
+
+            case R.id.ibPlay:
+                Intent play = new Intent(this, GameActivity.class);
+                startActivity(play);
+                break;
+
+            case R.id.ibScore:
+                Intent score = new Intent(this, ScoreActivity.class);
+                startActivity(score);
+                break;
+
+            case R.id.ibSettings:
+                Intent settings = new Intent(this, SettingsActivity.class);
+                startActivity(settings);
+                break;
+        }
     }
 }
