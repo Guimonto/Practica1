4773ae6072b6d1d5e6248d2c49cce6ab8c10f9f7
guimonto <guimonto@gmail.com>
2018-02-26 16:35:22 +0100

    Adapter i menu

--- app/src/main/java/sdm/pract1/whowantstobeamillionaire/ScoreActivity.java
+++ app/src/main/java/sdm/pract1/whowantstobeamillionaire/ScoreActivity.java
@@ -6,21 +6,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import sdm.pract1.whowantstobeamillionaire.adapter.ScoreAdapter;
+
 public class ScoreActivity extends AppCompatActivity {
 
     List<HighScore> ScoreListUser;
     List<HighScore> ScoreListFriends;
 
+    ListView scoreListViewUser;
+
+    ScoreAdapter adapter;
+
+    boolean clearUserScore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+        scoreListViewUser = (ListView) findViewById(R.id.user_score);
+
+        adapter = new ScoreAdapter(this, ScoreListUser, R.layout.score_list_row);
+
+        scoreListViewUser.setAdapter(adapter);
+
         TabHost host = (TabHost) findViewById(R.id.my_tab_host);
         host.setup();
         TabHost.TabSpec spec = host.newTabSpec(String.valueOf(R.id.tab1));
@@ -32,16 +49,25 @@ public class ScoreActivity extends AppCompatActivity {
         spec.setContent(R.id.tab2);
         host.addTab(spec);
 
+
         ScoreListUser = new ArrayList<>();
         ScoreListUser.addAll(getMockUserScore());
 
+
+
         ScoreListFriends = new ArrayList<>();
+
+        if(ScoreListUser.size() > 0)
+            clearUserScore = true;
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.score_menu, menu);
+
+        menu.findItem(R.id.delete_score).setVisible(clearUserScore);
+
         return true;
     }
 
@@ -61,9 +87,20 @@ public class ScoreActivity extends AppCompatActivity {
 
                         ScoreListUser.clear();
 
+                        adapter.notifyDataSetChanged();
+
+                        clearUserScore = false;
+
+                        supportInvalidateOptionsMenu();
+
                     }
                 });
+
+                builder.setNegativeButton(android.R.string.no, null);
+                builder.create().show();
+                return  true;
         }
+        return super.onOptionsItemSelected(item);
     }
 
     private List<HighScore> getMockUserScore(){
